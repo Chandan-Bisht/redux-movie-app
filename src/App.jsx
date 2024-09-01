af56dc7be96fa0f450d0b959f0569a78af55437a
Chandan Bisht <chandan.bisht@finouxad.com>
2024-09-01 11:50:46 +0530

    Adding routes to the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" index Component={Home} />
+          <Route path="/movie/:imdbID" Component={MovieDetail} />
+          <Route Component={PageNotFound} />
+        </Routes>
+        <Footer />
+    </div>
   )
 }
 
